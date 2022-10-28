1164937e892f54f18fdb31f3634df6e8ab6fb393
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-10-27 21:42:19 -0400

    refactor: :lipstick: update screen sizes for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "600px",
+      md: "900px",
+      lg: "1200px",
+      xl: "1536px",
+    },
     extend: {
       fontFamily: {
         sans: "Roboto, sans-serif",
