72e8593da50459e2837a77f7b753c14b3a124bff
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-11-15 16:15:02 -0300

    feat: :sparkles: create results page

--- src/components/layouts/primary/PrimaryLayout.tsx
+++ src/components/layouts/primary/PrimaryLayout.tsx
@@ -3,22 +3,27 @@ import { ReactNode } from "react";
 import { Footer } from "../../navigation/footer/Footer";
 import { Header } from "../../navigation/header/Header";
 
-export interface IPrimaryLayout {
+export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<"div"> {
   children: ReactNode;
+  justify?: "items-center" | "items-start";
 }
 
-export const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
+export const PrimaryLayout: React.FC<IPrimaryLayout> = ({
+  children,
+  justify = "items-center",
+  ...props
+}) => {
   return (
     <>
       <Head>
         <title>Google Clone</title>
       </Head>
-      <Header />
-      <main className="min-h-screen flex flex-col items-center">
-        {children}
-      </main>
-      <div className="m-auto" />
-      <Footer />
+      <div className={`min-h-screen flex flex-col ${justify}`} {...props}>
+        <Header />
+        <main className="px-5">{children}</main>
+        <div className="m-auto" />
+        <Footer />
+      </div>
     </>
   );
 };
